7f05ea290bc8af840028f30a0c37f970f8f04275
Ocelot <FinntheRaider@gmail.com>
2020-08-15 01:36:08 -0700

    Fixed effects moving over when there are no filters

--- src/main/java/com/mrcrayfish/filters/Hooks.java
+++ src/main/java/com/mrcrayfish/filters/Hooks.java
@@ -2,6 +2,7 @@ package com.mrcrayfish.filters;
 
 import net.minecraft.client.gui.DisplayEffectsScreen;
 import net.minecraft.client.gui.screen.inventory.CreativeScreen;
+import net.minecraft.item.ItemGroup;
 
 /**
  * Author: MrCrayfish
@@ -10,7 +11,7 @@ public class Hooks
 {
     public static int getPotionEffectOffset(DisplayEffectsScreen screen)
     {
-        if(screen instanceof CreativeScreen)
+        if (screen instanceof CreativeScreen && Filters.get().hasFilters(ItemGroup.GROUPS[((CreativeScreen) screen).getSelectedTabIndex()]))
         {
             return 172;
         }
@@ -19,7 +20,7 @@ public class Hooks
 
     public static int getEffectsGuiOffset(DisplayEffectsScreen screen)
     {
-        if(screen instanceof CreativeScreen)
+        if (screen instanceof CreativeScreen && Filters.get().hasFilters(ItemGroup.GROUPS[((CreativeScreen) screen).getSelectedTabIndex()]))
         {
             return 182;
         }
